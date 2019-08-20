0dc52901f93e5b1fd46c1f293ec8d5d965676633
Matthieu Mota <matthieumota@gmail.com>
2019-08-20 12:01:29 +0200

    Use annotations for routes

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class WelcomeController
 {
+    /**
+     * @Route("/hello", name="hello")
+     */
     public function hello()
     {
         $name = 'Matthieu';
