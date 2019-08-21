b758db8890a399438eadc9be6374082ec7ce4eff
pierrehervo <pierre.hervo@laposte.net>
2019-08-21 12:43:15 +0200

    TP controller super chaud

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,33 +2,66 @@
 
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    /**
-     * @Route("/product/{page}", name="product_list", requirements={"page"="\d+"})
+    public $products = [];
+
+    
+   public function __construct()
+   {
+       //On initialise un tableau avec des produits 
+       //L'attribut $products est accessible sur toutes les routes...
+        $this->products = [
+        ['name'=>'iphone 8','slug'=>'iphone-8','description'=>'portable de 2017','price'=>'990'],
+        ['name'=>'iphone 9','slug'=>'iphone-9','description'=>'portable de 2018','price'=> '1000'],
+        ['name'=>'iphone X','slug'=>'iphone-x','description'=>'portable de 2019','price'=>'1100']
+        
+       ];
+   }
+
+   /**
+     * @Route("/product/random")
      */
-    public function list($page)
+    public function random()
     {
-        return new Response('<body>Liste des produits page '.$page.'</body>');
+        //On ércupere une clé aléatoire du tableau
+        $key = array_rand($this->products);
+        //On récupére le product "random"
+        $product = $this->products[$key];
+
+        dump($product);
+
+        return $this->render('product/random.html.twig', ['product'=>$product]);
     }
 
     /**
-     * Si on se rend sur /product/toto ou /product/titi
-     * Un slug, c'est transformer "iPhone X" en "iphone-x"
-     *
-     * @Route("/product/{slug}", name="product_show")
+     * @Route("/product")
      */
-    public function show($slug, LoggerInterface $logger)
+    public function array()
     {
-        dump($slug);
-        // Je fais un log avec le service Monolog de Symfony
-        $logger->info('Visite du produit '.$slug);
+       $products = $this->products;
+        dump($products);
+        return $this->render('product/array.html.twig', ['products'=>$products]);
+    }
 
-        return $this->render('product/show.html.twig');
+    /**
+     * @Route("/product/{slug}")
+     */
+    public function product($slug)
+    {
+        //On va parcourir tous les produits
+        foreach ($this->products as $product){
+            //Si le slug du produit correspond à celui de l'URL
+            if ($product['slug']===$slug){
+                //Si un produit existe avec le slug, on retourne le template et on arrête le code
+                return $this->render('product/random.html.twig', ['product' =>$product,]);
+            }
+        }
+        //On s'assure de parcourir tout le tableau et seulement on affiche la 404
+        throw $this->createNotFoundException();
     }
 }
